e1aa8f3455e158124eefb69be04dab79952d9328
osamikoyo <v.pylayev@mail.ru>
2025-02-16 09:56:25 +0500

    add account models

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,38 @@
 package app
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func App() error {
+	var (
+		err error
+		input string
+		mainPass string = ""
+	)
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		input, err = reader.ReadString('\n')
+		if err != nil{
+			break
+		}
+
+		input = strings.TrimSpace(input)
+
+		if mainPass != "" {
+			
+		} else if strings.HasPrefix(input, "login") {
+			
+		} else if strings.HasPrefix(input, "create") {
+			
+		} else {
+			fmt.Println("you need to login\n please write `login [YOUR_MAINPASS]`")
+		}
+	}
+
+	return err
+}
--- internal/data/models.go
+++ internal/data/models.go
@@ -2,8 +2,14 @@ package data
 
 type Password struct{
 	ID uint `gorm:"primaryKey;autoIncrement"`
-	Pass string
+	Hash string
 	Login string
 	Theme string
 	Description string
+}
+
+type Account struct{
+	ID uint `gorm:"primaryKey;autoIncrement"`
+	Password string
+	Name string
 }
